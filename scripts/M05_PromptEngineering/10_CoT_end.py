c2653edebf28a3165acf57bc4f1754d263f75e26
GollnickDataSolutions <bert.gollnick@posteo.net>
2024-12-17 16:57:47 +0100

    CoT updated code based on langchain

--- scripts/M05_PromptEngineering/10_CoT_end.py
+++ scripts/M05_PromptEngineering/10_CoT_end.py
@@ -1,31 +1,31 @@
 #%% packages
-import openai
-from openai import OpenAI
-import pandas as pd
-import os
-openai.api_key = os.environ["OPENAI_API_KEY"]
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 from pprint import pprint
-# %%
-openai_client = OpenAI()
-
+from langchain_core.output_parsers import JsonOutputParser
+from pydantic import BaseModel, Field
 # %% Function Definition
-model_name = "gpt-3.5-turbo"
+MODEL_NAME = "gpt-4o-mini"
+model = ChatOpenAI(model=MODEL_NAME, temperature=0)
 
+class ChainOfThoughtResponse(BaseModel):
+    equation: str = Field(description="The answer to the user's question")
+
+#%% Function Definition
 def chain_of_thought(user_query:str):
-    system_role_definition = "You are a helpful assistent and answer precise and concise."
-    user_query_complete = f"{user_query}, think step-by-step."
+    system_role_definition = "You are a mathematic genius and can solve the game of 24. The goal of the Game of 24 is to use the four arithmetic operations (addition, subtraction, multiplication, and division) to combine four numbers and get a result of 24. Please check the final equation for correctness. Hints: Identify the basic operations, Prioritize multiplication and division, Look for combinations that make numbers divisible by 24, Consider order of operations, Use parentheses strategically, Practice with different number combinations. It is mandatory to use all four numbers. Only return the equation as JSON object with the following fields: equation. The user provides 4 numbers and you need to find the equation that solves the game of 24, think step-by-step. Please check the final equation if it equals 24."
+    user_query_complete = user_query
     messages = [
-        {"role": "system",
-         "content": system_role_definition},
-        {"role": "user", 
-         "content": user_query_complete}
+        ("system", system_role_definition),
+        ("user", user_query_complete)
     ]
-    response = openai_client.chat.completions.create(model=model_name, messages=messages)
-    content = response.choices[0].message.content
-    return content
+    prompt_template = ChatPromptTemplate.from_messages(messages)
+    chain = prompt_template | model | JsonOutputParser(pydantic_object=ChainOfThoughtResponse)
+    response = chain.invoke({})
+    return response
 
 #%% Test
-user_prompt = "The goal of the Game of 24 is to use the four arithmetic operations (addition, subtraction, multiplication, and division) to combine four numbers and get a result of 24. The numbers are 3, 4, 6, and 8. It is mandatory to use all four numbers. Please check the final equation for correctness. Hints: Identify the basic operations, Prioritize multiplication and division, Look for combinations that make numbers divisible by 24, Consider order of operations, Use parentheses strategically, Practice with different number combinations"
+user_prompt = "3, 4, 6, and 8"
 res = chain_of_thought(user_query=user_prompt)
 pprint(res)
 # %%
